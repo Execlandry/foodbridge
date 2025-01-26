67fdac6ac0c769c5e4e1417c33300bd58db9a859
Nikhil Kerkar <nikhilkerkar43@gmail.com>
2025-01-26 23:06:17 +0530

    added reverse proxy

---
+++ packages/proxy-service/src/app.module.ts
@@ -0,0 +1,29 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+
+import { AppService } from './app.service';
+import { ReverseProxyAuthMiddleware } from './middleware/proxy.auth.middleware';
+
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ReverseProxyAuthMiddleware)
+      .forRoutes({ path: 'v1/auth-service/*', method: RequestMethod.ALL });
+
+    /*
+  consumer
+    .apply(ReverseProxyAuthMiddleware)
+    .forRoutes({ path: 'v1/cart-service/*', method: RequestMethod.ALL });
+    */
+  }
+}
