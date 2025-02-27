cb5885768984bf4da7b1e3188a07c7f602f58e0b
Jeevesh23 <jeevesh.naidu2304@gmail.com>
2025-02-27 18:10:03 +0530

    business dashboard(another branch)

--- apps/business-dashboard/src/components/layout/Footer.js
+++ apps/business-dashboard/src/components/layout/Footer.js
@@ -5,7 +5,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center bg-slate-500 h-auto py-1 text-yellow-50 h-10 absolute w-full bottom-0 z-10">
-      <span className="">{new Date().getFullYear()} @ copy & fun :)</span>
+      <span className="block">{new Date().getFullYear()} © FoodBridge</span>
+      <span className="block">Empowering businesses to fight food waste</span>
     </div>
   );
 };
